2cfecd1a96c9d47d4c175e3e9856373628885faf
Aasthabarot <barotaastha38@gmail.com>
2024-06-18 10:18:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("hello aastha!!");
+
+
+let Songs=[
+    {songname:"Play Rain",filePath:"1.mp3",coverPath:"covers/1.jpg"},
+    {songname:"Love Clouds",filePath:"2.mp3",coverPath:"covers/2.jpg"},
+    {songname:"Run Away",filePath:"3.mp3",coverPath:"covers/3.jpg"},
+    {songname:"House of Lord",filePath:"4.mp3",coverPath:"covers/4.jpg"},
+    {songname:"Darling!!",filePath:"5.mp3",coverPath:"covers/5.jpg"},
+    {songname:"YOU",filePath:"6.mp3",coverPath:"covers/6.jpg"},
+    {songname:"Friends",filePath:"7.mp3",coverPath:"covers/7.jpg"},
+    {songname:"As You Cry",filePath:"8.mp3",coverPath:"covers/8.jpg"},
+    {songname:"Worthy",filePath:"9.mp3",coverPath:"covers/9.jpg"},
+    {songname:"Over And Over",filePath:"10.mp3",coverPath:"covers/10.jpg"},
+]
+let audioElement = new Audio("1.mp3");
